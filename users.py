a76376fc3a5c56d2d19bcd6593b1f415ce06d1ca
lcarlos <luiscardozoicg@gmail.com>
2023-01-18 20:29:23 -0500

    create endpoint for user

--- users.py
+++ users.py
@@ -6,19 +6,30 @@ from pydantic import BaseModel
 #Class User
 
 class User(BaseModel):
+    id: int
     name: str
     surname: str
     email: str
     age : int
 
 
-users_list = [User(name="luis",surname="lcarlos",email="luiscardozoicg@gmail.com",age=32),
-              User(name="carlos",surname="Carlos999",email="carlos@gmail.com",age=30),
-              User(name="Katalina",surname="Kata77",email="katalina@gmail.com",age=21),
-              User(name="Mauricio",surname="Maotapas",email="maotapas77@gmail.com",age=37)]
+users_list = [User(id = 1 ,name="luis",surname="lcarlos",email="luiscardozoicg@gmail.com",age=32),
+              User(id = 2 ,name="carlos",surname="Carlos999",email="carlos@gmail.com",age=30),
+              User(id = 3 ,name="Katalina",surname="Kata77",email="katalina@gmail.com",age=21),
+              User(id = 4 ,name="Mauricio",surname="Maotapas",email="maotapas77@gmail.com",age=37)]
 
 
 app = FastAPI()
 @app.get("/users")
 async def usersJson():
     return users_list
+
+
+@app.get("/user/{id}")
+async def userJson(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+
+    except: 
+        return {"Usuario no encontrado"}
